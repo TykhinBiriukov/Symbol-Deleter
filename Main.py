399c1f35ec211e23085c63c44141522fd3c36a55
Tykhin Biriukov <tybiriukov@gmail.com>
2024-05-24 01:45:17 +0300

    Minor code changes

--- Main.py
+++ Main.py
@@ -1,5 +1,11 @@
-with open("D:\Programming\Projects\Symbol Deleter\FourBitLogic.txt", "r") as file:
-    output = file.read().replace('|', "")
-    
-with open("D:\Programming\Projects\Symbol Deleter\Output2.txt", "w") as text_file:
+inputFile = input("Please enter way to the file you want to read: ")
+symbolToDelete = input("Please enter symbol to delete: ")
+symbolToReplace = input("Please enter with what to replace deleted symbol: ")
+#D:\Programming\Projects\Symbol Deleter\FourBitLogic.txt
+with open(inputFile, "r") as file:
+    output = file.read().replace(symbolToDelete, symbolToReplace)
+
+outputFile = input("Please enter way to the file in which you want to save output: ")
+#D:\Programming\Projects\Symbol Deleter\Output2.txt
+with open(outputFile, "w") as text_file:
     text_file.write(output)
